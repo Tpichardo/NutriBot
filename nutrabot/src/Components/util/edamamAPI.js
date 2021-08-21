a6410369c126bd2d398b1b252770637bb87d0d5c
Raymond Udeogu <rfizzle25@gmail.com>
2021-08-21 16:53:52 -0400

    recipe Api added to util

--- nutrabot/src/Components/util/edamamAPI.js
+++ nutrabot/src/Components/util/edamamAPI.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const foodApiKey = process.env.REACT_APP_FOOD_API_KEY;
 const foodApiId = process.env.REACT_APP_FOOD_API_ID;
+const recipeApiKey = process.env.REACT_APP_RECIPE_API_KEY;
+const recipeApiId = process.env.REACT_APP_RECIPE_API_ID;
 const nutritionAnalysisApiKey =
   process.env.REACT_APP_NUTRITION_ANALYSIS_API_KEY;
 const nutritionAnalysisApiId = process.env.REACT_APP_NUTRITION_ANALYSIS_API_ID;
@@ -13,8 +15,16 @@ const getProduct = async (input) => {
   return data.hints;
 };
 
+const getRecipe = async (input) => {
+  const { data } = await axios.get(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=${recipeApiId}&app_key=${recipeApiKey}`
+  );
+  return data.hits;
+};
+
 const functions = {
   getProduct,
+  getRecipe,
 };
 
 export default functions;
